7e89e5b5352317c44e4b2e3d7ad5240f59b5b7f1
mqnu00 <mqnu00@users.noreply.github.com>
2025-11-27 03:15:58 +0000

    deploy: 1c0eaeb5c6717d136727509ea8353c7d79eb454a

---
+++ assets/posts_vitepress_vitepress-tips.md.DgxAsaJT.js
@@ -0,0 +1,18 @@
+import{_ as a,c as n,o as e,ak as p}from"./chunks/framework.nCm-BxRp.js";const u=JSON.parse('{"title":"VitePress使用技巧","description":"","frontmatter":{"title":"VitePress使用技巧","date":"2025-01-16T00:00:00.000Z","author":"广习习","tags":["技术分享","VitePress","Markdown"]},"headers":[],"relativePath":"posts/vitepress/vitepress-tips.md","filePath":"posts/vitepress/vitepress-tips.md","lastUpdated":1764213283000}'),t={name:"posts/vitepress/vitepress-tips.md"};function i(l,s,o,r,c,d){return e(),n("div",null,[...s[0]||(s[0]=[p(`<h1 id="vitepress使用技巧" tabindex="-1">VitePress使用技巧 <a class="header-anchor" href="#vitepress使用技巧" aria-label="Permalink to &quot;VitePress使用技巧&quot;">​</a></h1><p>在这篇文章中，我想分享一些使用VitePress的实用技巧。</p><h2 id="markdown扩展功能" tabindex="-1">Markdown扩展功能 <a class="header-anchor" href="#markdown扩展功能" aria-label="Permalink to &quot;Markdown扩展功能&quot;">​</a></h2><p>VitePress提供了丰富的Markdown扩展功能：</p><h3 id="代码块高亮" tabindex="-1">代码块高亮 <a class="header-anchor" href="#代码块高亮" aria-label="Permalink to &quot;代码块高亮&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>\`\`\`js</span></span>
+<span class="line"><span>export default {</span></span>
+<span class="line"><span>  name: &quot;MyComponent&quot;,</span></span>
+<span class="line"><span>  data() {</span></span>
+<span class="line"><span>    return {</span></span>
+<span class="line"><span>      msg: &quot;Hello VitePress!&quot;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>  }</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span>\`\`\`</span></span></code></pre></div><h3 id="自定义容器" tabindex="-1">自定义容器 <a class="header-anchor" href="#自定义容器" aria-label="Permalink to &quot;自定义容器&quot;">​</a></h3><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>这是一个提示信息。</p></div><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>这是一个警告信息。</p></div><h3 id="代码行高亮" tabindex="-1">代码行高亮 <a class="header-anchor" href="#代码行高亮" aria-label="Permalink to &quot;代码行高亮&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>\`\`\`js{4}</span></span>
+<span class="line"><span>export default {</span></span>
+<span class="line"><span>  data() {</span></span>
+<span class="line"><span>    return {</span></span>
+<span class="line"><span>      msg: &quot;Highlighted!&quot;</span></span>
+<span class="line"><span>    }</span></span>
+<span class="line"><span>  }</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span>\`\`\`</span></span></code></pre></div><h2 id="主题定制" tabindex="-1">主题定制 <a class="header-anchor" href="#主题定制" aria-label="Permalink to &quot;主题定制&quot;">​</a></h2><p>VitePress允许你通过CSS变量来定制主题样式，这使得个性化变得非常简单。</p><p>希望这些技巧对你有帮助！</p>`,14)])])}const m=a(t,[["render",i]]);export{u as __pageData,m as default};
