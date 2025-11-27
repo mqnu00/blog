14fadccfce324e996421b42de3e929a5d4e4c57b
mqnu00 <647983952@qq.com>
2025-11-27 10:50:24 +0800

    feat: article tag

--- blog/.vitepress/theme/Layout.vue
+++ blog/.vitepress/theme/Layout.vue
@@ -2,19 +2,26 @@
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 import dayjs from 'dayjs'
+import { NTag } from 'naive-ui'
 
-const { page } = useData()
+const { page, frontmatter } = useData()
+console.log(frontmatter.value)
 </script>
 
 <template>
   <!-- 保留默认主题的布局 -->
   <DefaultTheme.Layout>
     <!-- 在标题区域插入更新时间 -->
-    <!-- <template #doc-before>
-      <p v-if="page.lastUpdated" class="last-updated">
+    <template #doc-before >
+      <div v-if="frontmatter.tags" style="display: flex; gap: 10px; margin-bottom: 10px;">
+        <NTag type="info" v-for="tag in frontmatter.tags">
+          {{tag}}
+        </NTag>
+      </div>
+      <!-- <p v-if="page.lastUpdated" class="last-updated">
         最后更新：{{ dayjs(page.lastUpdated).format('YYYY-MM-DD HH:mm') }}
-      </p>
-    </template> -->
+      </p> -->
+    </template>
   </DefaultTheme.Layout>
 </template>
 
