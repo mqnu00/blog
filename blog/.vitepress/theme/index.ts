3a52db6124b3d4edfb2a9602ca93f6175a5bca77
mqnu00 <647983952@qq.com>
2025-11-27 13:47:37 +0800

    chore: 挂载naiveui image

--- blog/.vitepress/theme/index.ts
+++ blog/.vitepress/theme/index.ts
@@ -3,11 +3,12 @@ import Layout from './Layout.vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import { NImage } from 'naive-ui'
 
 export default {
   extends: DefaultTheme,
   Layout,
   enhanceApp({ app, router, siteData }) {
-    // ...
+    app.component('NImage', NImage)
   }
 } satisfies Theme
