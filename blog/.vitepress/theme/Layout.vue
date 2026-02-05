9ff9ba11e3e3ce14e305de4abe92674b9f01090d
mqnu00 <647983952@qq.com>
2026-02-05 15:40:05 +0800

    feat: 添加文章编辑历史

--- blog/.vitepress/theme/Layout.vue
+++ blog/.vitepress/theme/Layout.vue
@@ -2,13 +2,13 @@
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 // import dayjs from 'dayjs'
-import { GlobalComponentConfig, GlobalThemeOverrides, NConfigProvider, NP, NTag } from 'naive-ui'
+import { dateZhCN, GlobalComponentConfig, GlobalThemeOverrides, NConfigProvider, NH1, NP, NTag, NTimeline, NTimelineItem, zhCN } from 'naive-ui'
 
-const { page, frontmatter } = useData()
+const { page, frontmatter} = useData()
 </script>
 
 <template>
-  <NConfigProvider >
+  <NConfigProvider :locale="zhCN" :date-locale="dateZhCN">
     <!-- 保留默认主题的布局 -->
     <DefaultTheme.Layout>
       <!-- 在标题区域插入更新时间 -->
@@ -30,6 +30,39 @@ const { page, frontmatter } = useData()
           最后更新：{{ dayjs(page.lastUpdated).format('YYYY-MM-DD HH:mm') }}
         </p> -->
       </template>
+      <template #doc-footer-before>
+        <div class="prev-next" v-if="page.git" style="display: flex; flex-direction: column; margin: 20px 0; gap: 10px;">
+          <div style="font-size: 14px; color: var(--vp-c-text-2);">
+            最后一次编辑:
+            <NTime :time="new Date(page.git.updated as string)" type="relative" />
+          </div>
+          <NCollapse 
+            :key="page.relativePath"
+            style="
+                background-color: rgb(233 233 238); 
+                border-radius: 8px; 
+                line-height: 30px;
+                padding: 10px;
+            "
+          >
+            <NCollapseItem title="查看所有历史" name="history">
+              <NTimeline>
+                <template v-for="history in page.git.history">
+                  <NTimelineItem
+                    type="info"
+                    :title="history.message"
+                    :time="history.date"
+                  >
+                    <a :href="history.url" target="_blank">
+                      <NTag style="cursor: pointer;" type="info" size="small">{{ history.sha.slice(0, 7) }}</NTag>
+                    </a>
+                  </NTimelineItem>
+                </template>
+              </NTimeline>
+            </NCollapseItem>
+          </NCollapse>
+        </div>
+      </template>
     </DefaultTheme.Layout>
   </NConfigProvider>
 </template>
@@ -40,4 +73,13 @@ const { page, frontmatter } = useData()
   color: #888;
   margin-top: -0.5em;
 }
+
+.prev-next {
+  border-top: 1px solid var(--vp-c-divider);
+  padding-top: 24px;
+  margin-top: 32px;
+  display: flex;
+  justify-content: space-between;
+}
+
 </style>
