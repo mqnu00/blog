71418bc8f4d9b74d910b457d845794803dd20763
mqnu00 <647983952@qq.com>
2026-04-02 20:10:17 +0800

    chore: 增加环境变量

--- blog/.vitepress/types/env.d.ts
+++ blog/.vitepress/types/env.d.ts
@@ -3,8 +3,10 @@
 interface ImportMetaEnv {
   readonly VITE_GITHUB_CLIENT_ID: string
   readonly VITE_GITHUB_REDIRECT_URI: string
+  readonly VITE_GITHUB_REPO_ID: string
   readonly VITE_GITHUB_DISCUSS_REP: string
   readonly VITE_GITHUB_DISCUSS_OWNER: string
+  readonly VITE_GITHUB_DISCUSS_TYPE_ID: string
   // 你可以继续加更多
   // readonly VITE_API_BASE: string
 }
